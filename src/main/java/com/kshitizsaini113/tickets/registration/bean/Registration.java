0e0464dff4f887ab0646475560dc8867398470cc
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2024-04-08 13:18:07 +0530

    Ticketing Application- Mongo DB - Soring Data

--- src/main/java/com/kshitizsaini113/tickets/registration/bean/Registration.java
+++ src/main/java/com/kshitizsaini113/tickets/registration/bean/Registration.java
@@ -2,9 +2,12 @@ package com.kshitizsaini113.tickets.registration.bean;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document("registrations")
 public record Registration(
-        Integer id,
+        @Id String id,
         @NotNull(message = "Product ID is required") Integer productId,
         String ticketCode,
         @NotBlank(message = "Attendee Name is required") String attendeeName
